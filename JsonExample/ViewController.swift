5ac631943c191d1390850b14d3b5a948a52bdb76
jaorsa <A01329978@itesm.mx>
2019-11-07 02:08:17 -0600

    Initial Commit

---
+++ JsonExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JsonExample
+//
+//  Created by Administrador on 07/11/19.
+//  Copyright © 2019 Jacky Ortiz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
